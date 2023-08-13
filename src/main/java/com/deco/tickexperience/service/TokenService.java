32f372b7c2b8b9e5198c134cfd1ec38bcc80519d
Wo-m <dom.thompson458@gmail.com>
2023-08-13 11:26:21 +1000

    comments

--- src/main/java/com/deco/tickexperience/service/TokenService.java
+++ src/main/java/com/deco/tickexperience/service/TokenService.java
@@ -11,17 +11,9 @@ import java.util.Random;
 public class TokenService {
 
     private final static Map<String, User> tokens = new HashMap<>();
-    private final static Random random = new Random();
-    private final static int MAX_INT = 1000000;
-
-    /*
-    TODO map token to user and last access time, and kill tokens
-        after certain inactive period (10 minutes?). Just
-        check in the filter and kill the token there.
-     */
 
     public String generateToken(User user) {
-//        String token = String.valueOf(random.nextInt(MAX_INT));
+        // FIXME need to do this properly
         String token = user.getUsername();
         tokens.put(token, user);
         return token;
