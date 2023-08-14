d2a906533e059a4e8e539a27541dc200dd8a7496
Wo-m <dom.thompson458@gmail.com>
2023-08-14 14:27:18 +1000

    post request body

---
+++ src/main/resources/db/changelog/dummy_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO User (Username, Password) VALUES ('dom', 'test');
+INSERT INTO Session (Location, StartTime, EndTime) VALUES ('Brisbane', '2019-10-10 10:00:00', '2019-10-10 12:00:00');
+INSERT INTO Ticket (UserId, SessionId, SeatNumber, Venue) VALUES (1, 1, 1, 'Brisbane');
