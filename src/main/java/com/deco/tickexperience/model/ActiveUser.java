d60edc43b60869b8633ddde65907ef9659df4440
Wo-m <dom.thompson458@gmail.com>
2023-08-29 10:18:56 +1000

    remove comment

--- src/main/java/com/deco/tickexperience/model/ActiveUser.java
+++ src/main/java/com/deco/tickexperience/model/ActiveUser.java
@@ -1,6 +1,5 @@
 package com.deco.tickexperience.model;
 
-import com.deco.tickexperience.model.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,6 +8,6 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 public class ActiveUser {
-    private Long userId; // may be better to just store name and pull from db
+    private Long userId;
     private LocalDateTime lastActiveTime;
 }
