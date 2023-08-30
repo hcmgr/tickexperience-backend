de04750eb1f125491ac7f8feeec0b865749acbd1
hcmgr <harryrackemann@gmail.com>
2023-08-30 17:40:56 +1000

    implemented sport service

---
+++ src/main/java/com/deco/tickexperience/repository/SportRepository.java
@@ -0,0 +1,6 @@
+package com.deco.tickexperience.repository;
+import com.deco.tickexperience.model.entity.Sport;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SportRepository extends CrudRepository<Sport, Long> {
+}
