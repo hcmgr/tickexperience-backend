c69dd8c477295b40829a926540334a5bf35c8688
hcmgr <harryrackemann@gmail.com>
2023-09-02 16:25:28 +1000

    removed 'Session' object from schema and codebase in general.

---
+++ src/main/java/com/deco/tickexperience/repository/VenueRepository.java
@@ -0,0 +1,7 @@
+package com.deco.tickexperience.repository;
+
+import com.deco.tickexperience.model.entity.Venue;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VenueRepository extends CrudRepository<Venue, Long> {
+}
