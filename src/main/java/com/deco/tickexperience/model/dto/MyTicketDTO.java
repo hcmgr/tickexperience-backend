2f8d41de6c3d46414a005f438e301f7d53d17228
Sam Smeaton <sam.smeaton@yousmartthing.com>
2023-09-07 18:17:03 +1000

    getMyTickets function

---
+++ src/main/java/com/deco/tickexperience/model/dto/MyTicketDTO.java
@@ -0,0 +1,19 @@
+package com.deco.tickexperience.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class MyTicketDTO {
+    private Long ticketId;
+    private LocalDateTime eventDateTime;
+    private String eventName;
+    private String venueName;
+    private String imageUrl;
+}
+
